a73ec5430b507e5850f728d563886ee11bb6af65
Michael Cao <mike.x.cao@gmail.com>
2016-10-02 11:29:25 -0400

    Added Webhooks

---
+++ plugins/webhook.js
@@ -0,0 +1,15 @@
+var config = require('../config.json');
+var request = require('request');
+var webhook = config.webhook;
+
+module.exports = {
+	main: function(bot, message) {
+		request.post({
+			url: webhook,
+			json:{
+				//"username": "Server Overlord",
+				"content": message.content
+			}
+		});
+	}
+};
