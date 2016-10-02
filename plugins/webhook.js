cb1cf06dfb4ad207cae71d43ba6dc2490c8ba763
Michael Cao <mike.x.cao@gmail.com>
2016-10-02 12:40:48 -0400

    Messed Up

--- plugins/webhook.js
+++ plugins/webhook.js
@@ -4,18 +4,13 @@ var webhook = config.webhook;
 
 module.exports = {
 	main: function(bot, message) {
-		var options = {
-			username: "Server Overlord",
-			content: message.content
-		}
-		
 		const isCommander = ["171319044715053057", "180094452860321793"];
 		if (isCommander.indexOf(message.author.id) > -1){
 			unirest.post(webhook)
-				.send({"username": "Server Overlords", "content": message.content})
-				.end(function (response) {
-					console.log(response.body);
-				});
+                .send({"username": "Server Overlords", "content": message.content})
+                .end(function (response) {
+                    console.log(response.body);
+                });
 			message.delete();
 		}
 	}
