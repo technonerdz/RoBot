917d27228f68e943295acf89f415beb11f300a6f
Michael Cao <mike.x.cao@gmail.com>
2016-10-02 20:58:55 -0400

    Hopefully fixed everything

--- plugins/announce.js
+++ plugins/announce.js
@@ -4,7 +4,7 @@ module.exports = {
 	main: function(bot, message) {
 		const isCommander = ["171319044715053057", "180094452860321793"];
 		if (isCommander.indexOf(message.author.id) > -1){
-			unirest.post('https://canary.discordapp.com/api/webhooks/232179498030858241/C_DigDr4xDqBLdJybpZb-y1S93rjkssSbK62QS2xiPsRZk-i8RWMRGqnp9feAFgPdUlZ1')
+			unirest.post('https://canary.discordapp.com/api/webhooks/232179498030858241/C_DigDr4xDqBLdJybpZb-y1S93rjkssSbK62QS2xiPsRZk-i8RWMRGqnp9feAFgPdUlZ')
                 .send({"username": "Server Administrators", "content": message.content})
                 .end(function (response) {
                     console.log(response.body);
