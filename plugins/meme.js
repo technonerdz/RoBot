fc2c6a75b545a21505800061a45956cfb777914a
Michael Cao <mike.x.cao@gmail.com>
2016-11-07 19:46:47 -0500

    Unneeded

--- plugins/meme.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	main: function(bot, message) {
-		var content = message.content.split(" ").splice(1).join(" ");
-		var text = content.split("-");
-
-		message.channel.sendMessage("http://memegen.link/" + text[0] + "/" + text[1].split(" ").join("-") + "/" + text[2].split(" ").join("-") + ".jpg");
-	}
-};
