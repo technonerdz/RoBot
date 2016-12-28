f53112de643d35ed42611490a7fc3fcb5bc1764c
Michael Cao <mike.x.cao@gmail.com>
2016-12-27 19:31:48 -0500

    Fixed give/take

--- plugins/take.js
+++ plugins/take.js
@@ -1,7 +1,7 @@
 module.exports = {
 	main: function(bot, message) {
 		const isCommander = ["171319044715053057", "180094452860321793"];
-		if (msg.member.hasPermission('MANAGE_ROLES_OR_PERMISSIONS') === true || msg.member.hasPermission('ADMINISTRATOR') === true || isCommander.indexOf(message.author.id) > -1) {
+		if (message.member.hasPermission('MANAGE_ROLES_OR_PERMISSIONS') === true || message.member.hasPermission('ADMINISTRATOR') === true || isCommander.indexOf(message.author.id) > -1) {
 			var user = message.mentions.users.array()[0];
 			var roleToTake = message.content.split(" ").splice(1).join(" ");
 			var role = message.guild.roles.find("name", roleToTake);
