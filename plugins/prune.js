2653b5f97d1b2d41d97e2d956ae17527bada7d71
Michael Cao <mike.x.cao@gmail.com>
2016-12-27 19:38:31 -0500

    Fixed prune/restart

--- plugins/prune.js
+++ plugins/prune.js
@@ -1,15 +1,16 @@
 module.exports = {
 	main: function(bot, message) {
-		if (message.member.roles.exists("name", "Bot Commander")){
+		if (message.member.hasPermission('MANAGE_MESSAGES') === true || isCommander.indexOf(message.author.id) > -1) {
 			var num = message.content;
 			if (!isNaN(num)) {
 				message.channel.fetchMessages({limit: num})
 					.then(messages => message.channel.bulkDelete(messages))
 					.catch(message.channel.bulkDelete);
 
-				message.channel.sendMessage("Deleted " + num + " messages under request of <@" + message.author.id + ">");
+				message.channel.sendMessage("Deleted " + num + " messages under request of <@" + message.author.id + ">")
+				.then(msg => setTimeout(function() {msg.delete();}, 5000);)
 			} else {
-				message.channel.sendMessage("Please specify a number");
+				message.channel.sendMessage("Please specify a number!");
 			}
 		}
 	}
--- plugins/restart.js
+++ plugins/restart.js
@@ -1,6 +1,6 @@
 module.exports = {
 	main: function(bot, message){
-		if (message.member.roles.exists("name", "Bot Commander")) {
+		if (message.member.hasPermission('ADMINISTRATOR') === true || isCommander.indexOf(message.author.id) > -1) {
 			message.channel.sendMessage(":wave: RoBot is restarting...");
 
 			setTimeout(function() {
