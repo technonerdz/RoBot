ef54845277e25727f9854388e79bc84d2db0ca21
Michael Cao <mike.x.cao@gmail.com>
2017-01-02 22:50:20 -0500

    TBA UPDATES

--- plugins/tba.js
+++ plugins/tba.js
@@ -60,8 +60,14 @@ module.exports = {
 						}
 						for(var j = 0; j < awards.length; j++) {
 							if(awards[j] != undefined) {
-								embed.addField("", awards[j])
-								.setColor(0x1675DB)
+								if(awards.length == 1) {
+									embed.addField("Award List", awards[j])
+									.setColor(0x1675DB)
+								}
+								else {
+									embed.addField("Award List Page " + (j + 1), awards[j])
+									.setColor(0x1675DB)
+								}
 								m.channel.sendEmbed(embed);
 								embed = null;
 							}
