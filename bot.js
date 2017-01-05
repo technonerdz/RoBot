e2cb858c27bfc2d1578113ff52c396244cbc7076
Michael Cao <mike.x.cao@gmail.com>
2017-01-05 18:40:01 -0500

    Logging Error

--- bot.js
+++ bot.js
@@ -63,7 +63,7 @@ bot.on("message", (msg) => {
       var year = new Date().getFullYear();
       db.serialize(function() {
         db.run(`CREATE TABLE IF NOT EXISTS frc_logs_${month}_${day - 1}_${year} (MSGINDEX INTEGER PRIMARY KEY, TIME DATETIME DEFAULT CURRENT_TIMESTAMP, CHANNEL_ID VARCHAR(32) NOT NULL, CHANNEL_NAME VARCHAR(32) NOT NULL, AUTHOR_ID VARCHAR(32) NOT NULL, AUTHOR_NAME VARCHAR(32) NOT NULL, AUTHOR_NICKNAME VARCHAR(32) NOT NULL, MESSAGE VARCHAR(2000) NOT NULL)`);
-        var stmt = db.prepare(`INSERT INTO frc_logs_${month}_${day - 1}_${year} (CHANNEL_ID, CHANNEL_NAME, AUTHOR_ID, AUTHOR_NAME, AUTHOR_NICKNAME, MESSAGE) VALUES (?, ?, ?, ?)`);
+        var stmt = db.prepare(`INSERT INTO frc_logs_${month}_${day - 1}_${year} (CHANNEL_ID, CHANNEL_NAME, AUTHOR_ID, AUTHOR_NAME, AUTHOR_NICKNAME, MESSAGE) VALUES (?, ?, ?, ?, ?, ?)`);
         var channelID = msg.channel.id, channelName = msg.channel.name, authorID = msg.author.id, authorNAME = msg.author.username, authorNICK = msg.author.nickname, message = msg.cleanContent;
         stmt.run(channelID, channelName, authorID, authorNAME, authorNICK, message);
         stmt.finalize();
