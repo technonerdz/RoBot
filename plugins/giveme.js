3d891e55a594fe25c07a8a9b3207c1cac9a9cd20
asianboiFRC <mike.x.cao@gmail.com>
2017-01-16 15:40:12 -0500

    Added Giveme

---
+++ plugins/giveme.js
@@ -0,0 +1,24 @@
+module.exports = {
+	main: function(bot, msg) {
+		const allowedRoles = ['Gaming', 'Wiring', 'Build', 'Programming', 'Marketing', 'Design', 'Awards', 'Strategy', 'Politics', 'Java', 'C++', 'LabVIEW', 'Python'];
+		var roleToGive = msg.content;
+		roleToGive = roleToGive.trim().toLowerCase();
+		var found = false;
+		var role = null;
+		for(var i = 0; i < allowedRoles.length; i++) {
+			if(allowedRoles[i].toLowerCase() == roleToGive) {
+				found = true;
+				role = msg.guild.roles.find("name", allowedRoles[i]);
+			}
+		}
+		if (found == true) {
+			var member = msg.member;
+			member.addRole(role).catch(console.error);
+			msg.channel.sendMessage("Successfully gave you " + role.name + "!");
+		} else if(roleToGive == "list") {
+			
+		} else {
+			msg.reply('that giveme does not exist!');
+		}
+	}
+};
