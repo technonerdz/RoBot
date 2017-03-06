43d7426b84f75e484a84a38eb8b58351a744630c
asianboiFRC <mike.x.cao@gmail.com>
2017-03-05 22:29:45 -0500

    Fighting Command

--- plugins/give.js
+++ plugins/give.js
@@ -1,18 +1,18 @@
 module.exports = {
-	main: function(bot, message) {
+	main: function(bot, msg) {
 		const isCommander = ["171319044715053057", "180094452860321793"];
-		if (message.member.hasPermission('MANAGE_ROLES_OR_PERMISSIONS') === true || message.member.hasPermission('ADMINISTRATOR') === true || isCommander.indexOf(message.author.id) > -1) {
-			var user = message.mentions.users.array()[0];
-			var roleToGive = message.content.split(" ").splice(1).join(" ");
+		if (msg.member.hasPermission('MANAGE_ROLES_OR_PERMISSIONS') === true || msg.member.hasPermission('ADMINISTRATOR') === true || isCommander.indexOf(msg.author.id) > -1) {
+			var user = msg.mentions.users.array()[0];
+			var roleToGive = msg.content.split(" ").splice(1).join(" ");
 			roleToGive = roleToGive.trim();
-			let role = message.guild.roles.find("name", roleToGive);
+			let role = msg.guild.roles.find("name", roleToGive);
 			if (!role) {
-				message.channel.sendMessage("Role does not exist.");
+				msg.channel.sendMessage("Role does not exist.");
 				return;
 			}
-			var member = message.guild.members.get(user.id);
+			var member = msg.guild.members.get(user.id);
 			member.addRole(role).catch(console.error);
-			message.channel.sendMessage("Successfully added role " + roleToGive + " to " + user.username + ".");
+			msg.channel.sendMessage("Successfully added role " + roleToGive + " to " + user.username + ".");
 		}
 	}
 };
