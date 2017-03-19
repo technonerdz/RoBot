6255cb4b533a49868d373a306125082472e488e5
asianboiFRC <mike.x.cao@gmail.com>
2017-03-19 11:47:10 -0400

    Timeout

---
+++ plugins/eval.js
@@ -0,0 +1,56 @@
+module.exports = {
+	main: function(bot, msg) {
+		var Discord = require('discord.js');
+		const util = require('util');
+		
+		if (msg.author.id === "171319044715053057" || msg.member.hasPermission('ADMINISTRATOR')) {
+			var code = msg.content;
+			var embed = new Discord.RichEmbed();
+			try {
+				let evaled = eval(code);
+				let type = typeof evaled;
+				let insp = util.inspect(evaled, {
+					depth: 0
+				});
+
+				if (evaled === null) evaled = 'null';
+
+				embed.setColor(0x00FF00)
+				.setTitle("RoBot Javascript Evaluation Complete")
+				.setFooter(`${msg.author.username}`, `${bot.user.avatarURL}`)
+				.setTimestamp()
+				.addField('Code', "```js\n" + clean(code) + "```")
+				.addField('Result', "```js\n" + clean(evaled.toString().replace(bot.token, 'REDACTED')) + "```");
+				if (evaled instanceof Object) {
+					embed.addField('Inspect', "```js\n" + insp.toString().replace(bot.token, 'REDACTED') + "```");
+				} else {
+					embed.addField('Type', "```js\n" + type + "```");
+				}
+				msg.delete();
+				msg.channel.sendEmbed(embed)
+			} catch (err) {
+				embed.setColor(0xFF0000)
+				.setTitle(":rotating_light: :rotating_light: ERROR ERROR ERROR :rotating_light: :rotating_light: in RoBot Javascript Evaluation")
+				.setFooter(`${msg.author.username}`, `${bot.user.avatarURL}`)
+				.setTimestamp()
+				.addField('Code', "```js\n" + clean(code) + "```")
+				.addField('Error', "```LDIF\n" + clean(err.message) + "```");
+				msg.delete();
+				msg.channel.sendEmbed(embed)
+					.catch(error => console.log(error.stack));
+			}
+		}
+		else {
+			msg.channel.sendMessage(msg.author + ", you do not have permission to use eval!");
+		}
+		
+		function clean(text) {
+			if (typeof(text) === "string") {
+				return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+			}
+			else {
+				return text;
+			}
+		}
+	}
+}
