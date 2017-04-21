e2c505c61c89b000e375cdb574a3c92136978430
Michael Cao <mike.x.cao@gmail.com>
2017-04-20 21:41:51 -0400

    Feedback

--- plugins/feedback.js
+++ plugins/feedback.js
@@ -4,6 +4,7 @@ module.exports = {
     permission: 1,
     help: 'Allows the user to provide feedback about the bot',
 	main: function(bot, msg) {
+		msg.reply("your feedback has been recieved!");
 		var owner = bot.users.get('171319044715053057');
 		var f = new Discord.RichEmbed();
 		f.setColor(0x1675DB)
