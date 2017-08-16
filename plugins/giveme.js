1e498951976573ff9cbab67b5e5e01757f44a471
Michael Cao <mike.x.cao@gmail.com>
2017-08-16 11:27:38 -0400

    Removed giveme command

--- plugins/giveme.js
+++ plugins/giveme.js
@@ -4,14 +4,38 @@ module.exports = {
 	permission: 1,
 	help: 'Gives the user a specified role from an allowed list.',
 	main: function(bot, msg) {
-		var allowedRoles = [];
+		return msg.channel.send("Hey! Please use RoBot Beta's giveme command instead! It's a lot better!")
+		
 		if (msg.guild.id == '176186766946992128') {
 			allowedRoles = ['Gaming', 'Wiring', 'Build', 'Programming', 'Marketing', 'Design', 'Awards', 'Strategy', 'Politics', 'Java', 'C++', 'LabVIEW', 'Python', 'Media', 'CS:GO', 'Civilization', 'CAH', 'League of Legends', 'Overwatch', 'Rocket League', 'Minecraft', 'ChezyChamps', 'Hackathon', 'Alumni', 'Solidworks', 'Inventor', 'Creo', 'Social'];
 		} else if (msg.guild.id == '225450307654647808') {
 			allowedRoles = ['Social'];
 		}
-		var roleToGive = msg.content.trim().toLowerCase();
-		var found = false;
+		var cmd = msg.content.split(" ")[0];
+		
+		if(cmd == "list" | cmd == "-l") {
+			var list = "List of Allowed Roles:";
+			for (var i = 0; i < allowedRoles.length; i++)
+				list += "\n" + allowedRoles[i];
+			msg.channel.send(list);
+		}
+		
+		var allowedRoles = [];
+		var assigned = "";
+		var assignedCount = 0;
+		var alreadyHad = "";
+		var alreadyHadCount = 0;
+		var notFoundCount = 0;
+		if(msg.content.indexOf("|") > -1) {
+			var 
+			for(var i = 0; i < )
+		}
+		else {
+			var roleToGive = msg.content.trim().toLowerCase(),
+				found = false,
+				role = null;
+			
+		}
 		var role = null;
 		for (var i = 0; i < allowedRoles.length; i++) {
 			if (allowedRoles[i].toLowerCase() == roleToGive) {
@@ -24,10 +48,7 @@ module.exports = {
 			member.addRole(role).catch(console.error);
 			msg.channel.send("Successfully gave you " + role.name + "!");
 		} else if (roleToGive == "list" || roleToGive == "-l") {
-			var list = "List of Allowed Roles:";
-			for (var i = 0; i < allowedRoles.length; i++)
-				list += "\n" + allowedRoles[i];
-			msg.channel.send(list);
+			
 		} else {
 			msg.reply('that giveme does not exist!');
 		}
